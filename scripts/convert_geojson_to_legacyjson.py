2c31d300d5cf8af1086256793481feaf7d910131
simonpoole <simon@poole.ch>
2016-11-14 22:18:54 +0100

    Add generated bbox to legacy output

--- scripts/convert_geojson_to_legacyjson.py
+++ scripts/convert_geojson_to_legacyjson.py
@@ -1,42 +1,36 @@
-import json, sys, io, argparse
+import json, sys, io
 
-def convert_json_source(args, source):
+def convert_json_source(source):
     converted = {}
     extent_obj = {}
 
     geometry = source.get('geometry') or {}
     polygon_coords = geometry.get('coordinates') or []
     if polygon_coords:
-        if args.gen_bbox:
-            # extent_obj['polygon'] = polygon_coords
-            # generate bbox from polygon coordinates as a stop gap
-            min_lon = 180
-            max_lon = -180
-            min_lat = 90
-            max_lat = -90
-            for ring in polygon_coords:
-                for coord in ring:
-                    if coord[0] < min_lon:
-                        min_lon = coord[0]
-                    if coord[0] > max_lon:
-                        max_lon = coord[0]
-                    if coord[1] < min_lat:
-                        min_lat = coord[1]
-                    if coord[1] > max_lat:
-                        max_lat = coord[1]
-            bbox_obj = {}
-            bbox_obj['min_lon'] = min_lon
-            bbox_obj['max_lon'] = max_lon
-            bbox_obj['min_lat'] = min_lat
-            bbox_obj['max_lat'] = max_lat
-            extent_obj['bbox'] = bbox_obj
-        if not args.remove_polygons:
-            extent_obj['polygon'] = polygon_coords
+        extent_obj['polygon'] = polygon_coords
+        # generate bbox from polygon coordinates as a stop gap         
+        min_lon = 180
+        max_lon = -180
+        min_lat = 90
+        max_lat = -90
+        for ring in polygon_coords:
+            for coord in ring:
+                if coord[0] < min_lon:
+                    min_lon = coord[0]
+                if coord[0] > max_lon:
+                    max_lon = coord[0]
+                if coord[1] < min_lat:
+                    min_lat = coord[1]
+                if coord[1] > max_lat:
+                    max_lat = coord[1]
+        bbox_obj = {}
+        bbox_obj['min_lon'] = min_lon
+        bbox_obj['max_lon'] = max_lon
+        bbox_obj['min_lat'] = min_lat
+        bbox_obj['max_lat'] = max_lat
+        extent_obj['bbox'] = bbox_obj
 
     properties = source.get('properties') or {}
-    if args.tms_only and properties['type'] == 'wms':
-        return {}
-
     for f in ['name', 'type', 'url', 'license_url', 'id', 'description',
             'country_code', 'default', 'best', 'start_date', 'end_date',
             'overlay', 'available_projections', 'attribution', 'icon']:
@@ -54,17 +48,18 @@ def convert_json_source(args, source):
 
     return converted
 
-parser = argparse.ArgumentParser(description='Generate legacy json output format from geojosn format sources')
-parser.add_argument('files', metavar='F', nargs='+', help='file(s) to process')
-parser.add_argument('-b', dest='gen_bbox', action='store_true', help='generate bounding boxes from polygons')
-parser.add_argument('-t', dest='tms_only', action='store_true', help='only include tile servers')
-parser.add_argument('-r', dest='remove_polygons', action='store_true', help='remove polygons from output, typically used together with -b')
-
-args = parser.parse_args()
-
 features = []
-for file in args.files:
+for file in sys.argv[1:]:
     with io.open(file, 'r') as f:
-        features.append(convert_json_source(args, json.load(f)))
+        features.append(convert_json_source(json.load(f)))
 
+<<<<<<< Upstream, based on branch 'gh-pages' of https://github.com/osmlab/editor-layer-index.git
 print(json.dumps(features, sort_keys=True, separators=(',', ':'), ensure_ascii=False).encode('utf-8'))
+=======
+print(json.dumps(
+    features,
+    indent=4,
+    sort_keys=True,
+    separators=(',', ': ')
+))
+>>>>>>> dcd33c7 Add generated bbox to legacy output
